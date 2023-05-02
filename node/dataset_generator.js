4b62a7d021e324c82a7b48409921a5aa65b583ad
pkro <pkrombach@web.de>
2023-05-02 11:56:07 +0200

    revert to js complete.

--- node/dataset_generator.js
+++ node/dataset_generator.js
@@ -1,26 +1,17 @@
-import * as fs from 'fs';
-import draw from './draw';
-import { createCanvas } from 'canvas';
-
-console.log(draw);
-
-const constants: Record<string, string> = {};
-constants.DATA_DIR = "../data";
-constants.RAW_DIR = constants.DATA_DIR + "/raw";
-constants.DATASET_DIR = constants.DATA_DIR + "/dataset";
-constants.JSON_DIR = constants.DATASET_DIR + "/json";
-constants.IMG_DIR = constants.DATA_DIR + "/img";
-constants.SAMPLES = constants.DATASET_DIR + "/samples.json";
+const fs = require('fs');
+const draw = require('../common/draw');
+const constants = require('../common/constants');
+const { createCanvas } = require('canvas');
 
 const canvas =createCanvas(400,400);
-const ctx: any = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 
 const fileNames = fs.readdirSync(constants.RAW_DIR);
-const samples: any[] = [];
+const samples = [];
 
 let id = 1;
 
-fileNames.forEach((fn: string) => {
+fileNames.forEach((fn) => {
     const content = fs.readFileSync(constants.RAW_DIR + "/" + fn);
 
     const {session, student, drawings} = JSON.parse(content.toString());
@@ -35,7 +26,7 @@ fileNames.forEach((fn: string) => {
         });
 
         // data files with the drawing arrays
-        const paths: Array<Array<[number, number]>> = drawings[label];
+        const paths = drawings[label];
         fs.writeFileSync(constants.JSON_DIR + "/" + id + ".json", JSON.stringify(paths));
 
         generateImageFile(constants.IMG_DIR + "/" + id + ".png", paths);
@@ -47,7 +38,7 @@ fileNames.forEach((fn: string) => {
 // this is the mapping file between the data files and the labels (by id = data file label)
 fs.writeFileSync(constants.SAMPLES, JSON.stringify(samples, null, 2));
 
-function generateImageFile(outFile: string, paths: Array<Array<[number, number]>>): void {
+function generateImageFile(outFile, paths) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     draw.paths(ctx, paths);
     const buffer = canvas.toBuffer("image/png");
